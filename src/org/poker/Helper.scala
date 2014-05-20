8e4eb0db1f9813291c074700e0a0b0ba9e4e0cd1
Liviu Cristian Mirea Ghiban <contact@liviucmg.com>
2014-05-20 15:05:27 +0300

    Add methods for card names.

--- src/org/poker/Helper.scala
+++ src/org/poker/Helper.scala
@@ -3,6 +3,38 @@ package org.poker
 object Helper {
   val random = new scala.util.Random
 
-  def randomNumber(min: Int, max: Int):
-    Int = min + random.nextInt(max - min)
+  def randomNumber(min: Int, max: Int): Int = {
+    min + random.nextInt(max - min)
+  }
+
+  def getCardNumber(card: Int): Int = {
+    card % 13
+  }
+
+  def getCardColor(card: Int): Int = {
+    card % 4
+  }
+
+  def getCardName(card: Int): String = {
+    val number = {
+      getCardNumber(card) match {
+        case 9 => "Jack"
+        case 10 => "Queen"
+        case 11 => "King"
+        case 12 => "Ace"
+        case default => default + 2
+      }
+    }
+
+    val color = {
+      getCardColor(card) match {
+        case 0 => "Spades"
+        case 1 => "Hearts"
+        case 2 => "Diamonds"
+        case 3 => "Clubs"
+      }
+    }
+
+    number + " of " + color
+  }
 }
